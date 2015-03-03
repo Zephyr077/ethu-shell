10391ee492e9acfcd1f0803439459123ddc965df
Zephyr077 <eduar2.w@gmail.com>
2015-03-03 20:57:57 -0300

    Update prueba2.pp

--- codigo/prueba2.pp
+++ codigo/prueba2.pp
@@ -7,6 +7,6 @@ for j:= 1 to 10000 do
 begin
 Delay(500);
 writeln(j);
-writeln('Soy el proceso PRUEBA2 y Te digo mi PIDpor las dudas: ', fpgetpid);
+writeln('Soy el proceso PRUEBA2 y Te digo mi PID: ', fpgetpid);
 end;
 end.
